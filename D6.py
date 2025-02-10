7ddfd19fbef2a56c7f058f63fae35f4c094adf34
Reddonut-cyber <akhowsuk@gmail.com>
2025-02-10 18:20:22 +0700

    Implement forward elimination function and update matrix printing in D6.py

--- D6.py
+++ D6.py
@@ -3,7 +3,16 @@ import random
 def create_matrix(n):
     matrix = [[random.randint(1, 10) for _ in range(n)] for _ in range(n)]
     return matrix
+def forward_elimination():
+    matrix = create_matrix(n)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            print(matrix)
+            factor = matrix[j][i] / matrix[i][i]
+            for k in range(i, n):
+                matrix[j][k] -= factor * matrix[i][k]
+    return matrix
 
 n = random.randint(1, 10)
 print(n)
-print(f"{create_matrix(n)}")
+print(f"{forward_elimination()}")
